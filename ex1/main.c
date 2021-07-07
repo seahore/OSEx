04450941f536ccb5fe0ba288bbb2da4bc730fe59
seahore <seahore@whu.edu.cn>
2021-07-07 14:28:24 +0800

    ex1: modified output format

--- ex1/main.c
+++ ex1/main.c
@@ -7,17 +7,16 @@ void Run() {
     int cnt = 1;
     while(1) {
         printf("%d\t", cnt++);
+        printf("Ready queue: ");
+        for(PCB *iter = readyQueue;iter;iter=iter->next){
+            printf("%hu, ", iter->PID);
+        }
         --readyQueue->TimeRemaining;
         if (!readyQueue->TimeRemaining) {
-            printf("Process %hu has been completed. ", readyQueue->PID);
+            printf("process %hu runs and then be completed. ", readyQueue->PID);
             PCBDeque();
         } else {
-            printf("Process %hu: time remaining: %lu -> %lu, priority: %hhu -> %hhu ",
-                   readyQueue->PID,
-                   readyQueue->TimeRemaining + 1,
-                   readyQueue->TimeRemaining,
-                   readyQueue->Priority,
-                   readyQueue->Priority > 1 ? readyQueue->Priority - 1 : 1);
+            printf("process %hu runs. ", readyQueue->PID);
             PCBReenque();
         }
         if (!readyQueue) {
@@ -33,9 +32,9 @@ int main() {
     int cnt;
     printf("Enter the number of process: ");
     scanf("%d", &cnt);
-    if(cnt > 0) {
+    if (cnt > 0) {
         for (int i = 0; i < cnt; ++i) {
-            printf("Process %d (<PID> <Time> <Prior>): ", i);
+            printf("Register %d/%d (<PID> <Time> <Prior>): ", i + 1, cnt);
             Word pid;
             DWord time;
             unsigned int prior;
