995b70558cb08e0c74a7099de5bb4eb0f2438f69
seahore <seahore@whu.edu.cn>
2021-07-10 11:24:15 +0800

    ex3: modified bitmap printing format

--- ex3/main.c
+++ ex3/main.c
@@ -4,9 +4,12 @@
 #include "DiskManaging.h"
 
 void PrintBitmap(){
+    puts("   0 1 2 3 4 5 6 7");
     for(int i=0; i<8; ++i) {
+        printf("%d: ", i);
         for(int j=0; j<8; ++j) {
             putchar('0' + ((bitmap[i] >> j) % 2));
+            putchar(' ');
         }
         putchar('\n');
     }
